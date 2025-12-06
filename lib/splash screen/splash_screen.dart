4648b02ec7cbd8017bc1d7b8b50a3a7e8ad3b2d3
Hx-313 <aliabdullahva313@gmail.com>
2025-12-06 09:34:44 +0500

    first commit

---
+++ lib/splash screen/splash_screen.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:snapcheck/auth_gate.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _animationController;
+  late final Animation<double> _animation;
+
+  void animation() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 2200),
+    );
+    _animation =
+        Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
+    _animationController.forward();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    animation();
+    Future.delayed(Duration(milliseconds: 3000), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => AuthGate(),
+        ),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF496ACB),
+              Color(0xFFA175DF),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32),
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image.asset('assets/images/finalLogo.png'),
+                        const SizedBox(height: 10),
+                        Text(
+                          'Parental Control System',
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontFamily: 'Inter',
+                            color: Colors.white,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                /// bottom navigation bar
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 40),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: AnimatedBuilder(
+                      animation: _animation,
+                      builder: (context, child) {
+                        return LinearProgressIndicator(
+                          value: _animation.value,
+                          backgroundColor: Color(0xFF092868),
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.white),
+                          minHeight: 6,
+                          borderRadius: BorderRadius.circular(3),
+                        );
+                      },
+                    ), // custom widget with 3s animation
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

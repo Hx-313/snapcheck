3c86348d31146a36fd8c94f00e5713b66637b2d4
Hx-313 <aliabdullahva313@gmail.com>
2025-12-08 16:44:43 +0500

    Refactor Dashboard layout; replace Stack with Column and add button for monitoring child's device

--- lib/home/ui/pages/dashboard.dart
+++ lib/home/ui/pages/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:snapcheck/Auth/ui/widgets/my_button.dart';
 import 'package:snapcheck/common/paddings/paddings.dart';
 
 class Dashboard extends StatefulWidget {
@@ -13,35 +14,56 @@ class _DashboardState extends State<Dashboard> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Stack(
-          children: [
-            Container(
-              width: double.infinity,
-              height: double.infinity,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFF3C8FF4),
-                    Color(0xFF9656ED),
-                  ],
-                  begin: Alignment.centerLeft,
-                  end: Alignment.centerRight,
-                ),
-              ),
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFF3C8FF4),
+                Color(0xFF9656ED),
+              ],
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
             ),
-            Positioned(
-              left: 20,
-              top: 50,
-              child: Container(
-                width: 45.0,
-                height: Paddings.reponsiveHeight(context, 45.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: Paddings.reponsiveHeight(context, 40.0),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SizedBox(
+                  height: Paddings.reponsiveHeight(context, 40.0),
+                  child: Image.asset('assets/images/finalLogo.png'),
                 ),
               ),
-            )
-          ],
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16.0),
+                      topRight: Radius.circular(16.0),
+                    ),
+                  ),
+                  child: Center(
+                    child: SizedBox(
+                        height: Paddings.reponsiveHeight(context, 60.0),
+                        width: MediaQuery.of(context).size.width * 0.75,
+                        child: MyButton(
+                          text: 'Monitor Child\'s Device',
+                          onTap: () {},
+                        )),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

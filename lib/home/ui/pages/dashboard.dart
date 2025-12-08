9fe0099b109587f242feb433a2ad2b004404b3fa
Hx-313 <aliabdullahva313@gmail.com>
2025-12-08 20:37:19 +0500

    Refactor Dashboard layout; update gradient background and button alignment for improved UI Add plugin registrations for battery_plus and connectivity_plus in macOS and Windows Update pubspec.yaml to include device_info_sdk dependency

--- lib/home/ui/pages/dashboard.dart
+++ lib/home/ui/pages/dashboard.dart
@@ -13,57 +13,59 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color(0xFF3C8FF4),
-                Color(0xFF9656ED),
-              ],
-              begin: Alignment.centerLeft,
-              end: Alignment.centerRight,
-            ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: Paddings.reponsiveHeight(context, 40.0),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: SizedBox(
-                  height: Paddings.reponsiveHeight(context, 40.0),
-                  child: Image.asset('assets/images/finalLogo.png'),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(16.0),
-                      topRight: Radius.circular(16.0),
-                    ),
-                  ),
-                  child: Center(
-                    child: SizedBox(
-                        height: Paddings.reponsiveHeight(context, 60.0),
-                        width: MediaQuery.of(context).size.width * 0.75,
-                        child: MyButton(
-                          text: 'Monitor Child\'s Device',
-                          onTap: () {},
-                        )),
-                  ),
-                ),
-              ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF3C8FF4),
+              Color(0xFF9656ED),
             ],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
           ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: Paddings.reponsiveHeight(context, 40.0),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SizedBox(
+                height: Paddings.reponsiveHeight(context, 40.0),
+                child: Image.asset('assets/images/finalLogo.png'),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16.0),
+                    topRight: Radius.circular(16.0),
+                  ),
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.75,
+                          child: MyButton(
+                            text: 'Monitor Child\'s Device',
+                            onTap: () {},
+                          )),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
